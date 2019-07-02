af3b9ae22ba76b0545cd810736ab9123a5e5a6e1
Olga Matsyk <olga.matsyk2016@gmail.com>
2019-07-02 17:30:47 +0300

    Created initial RedditPosts project.

---
+++ RedditPosts/RedditPosts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditPosts
+//
+//  Created by Olga Matsyk on 7/2/19.
+//  Copyright © 2019 Matsyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
