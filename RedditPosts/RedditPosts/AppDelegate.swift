e6a897e64640e9c128489738e195d7f0a6cc88d2
Olga Matsyk <olga.matsyk2016@gmail.com>
2019-07-02 17:53:22 +0300

    Removed Storyboard. Added initial UI programmatically.

--- RedditPosts/RedditPosts/AppDelegate.swift
+++ RedditPosts/RedditPosts/AppDelegate.swift
@@ -12,10 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            let mainVC = ViewController()
+            navigationController = UINavigationController(rootViewController: mainVC)
+            
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+
         return true
     }
 
@@ -40,7 +48,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- RedditPosts/RedditPosts/ViewController.swift
+++ RedditPosts/RedditPosts/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView : UITableView = {
+        let t = UITableView()
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // set a background color
+        self.view.backgroundColor = UIColor.init(white: 0.9, alpha: 0.9)
+        
+        // add the table view to self.view
+        self.view.addSubview(tableView)
+        
+        
+        // UI constraints
+        tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        
+        // set delegate and datasource
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // register a defalut cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
